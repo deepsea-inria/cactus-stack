7af3a8b7818ebd76944d9856906dcc2718d087df
Mike Rainey <mike.rainey@inria.fr>
2017-03-09 01:31:14 +0100

    Working on basic cactus stack

--- test/cactus-basic.cpp
+++ test/cactus-basic.cpp
@@ -22,6 +22,7 @@ namespace cactus_stack {
     class frame {
     public:
       int v;
+      parent_link_type plt;
     };
     
     /* Frame */
@@ -44,7 +45,7 @@ namespace cactus_stack {
     };
     
     bool is_marked(frame& f) {
-      return f.s.plt == Parent_link_async;
+      return f.plt == Parent_link_async;
     }
     
     fork_result_type fork_mark(reference_stack_type& s) {
@@ -84,35 +85,35 @@ namespace cactus_stack {
       trace_tag_type tag;
       struct {
         frame f;
-        std::unique_ptr<struct trace_struct> k;
+        std::shared_ptr<struct trace_struct> k;
       } push_back;
       struct {
-        std::unique_ptr<struct trace_struct> k;
+        std::shared_ptr<struct trace_struct> k;
       } pop_back;
       struct fork_mark_struct {
-        std::unique_ptr<struct trace_struct> k1;
-        std::unique_ptr<struct trace_struct> k2;
+        std::shared_ptr<struct trace_struct> k1;
+        std::shared_ptr<struct trace_struct> k2;
       } fork_mark;
     };
     
     using trace_type = struct trace_struct;
     
-    trace_type mk_push_back(frame f) {
-      trace_type t;
-      t.tag = Trace_push_back;
-      t.push_back.f = f;
+    trace_type* mk_push_back(frame f) {
+      trace_type* t = new trace_type;
+      t->tag = Trace_push_back;
+      t->push_back.f = f;
       return t;
     }
     
-    trace_type mk_pop_back() {
-      trace_type t;
-      t.tag = Trace_pop_back;
+    trace_type* mk_pop_back() {
+      trace_type* t = new trace_type;
+      t->tag = Trace_pop_back;
       return t;
     }
     
-    trace_type mk_fork_mark() {
-      trace_type t;
-      t.tag = Trace_fork_mark;
+    trace_type* mk_fork_mark() {
+      trace_type* t = new trace_type;
+      t->tag = Trace_fork_mark;
       return t;
     }
     
@@ -129,19 +130,116 @@ namespace cactus_stack {
     using machine_config_type = struct machine_config_struct {
       machine_tag_type tag;
       struct {
-        std::unique_ptr<struct machine_config_struct> m1;
-        std::unique_ptr<struct machine_config_struct> m2;
+        std::shared_ptr<struct machine_config_struct> m1;
+        std::shared_ptr<struct machine_config_struct> m2;
       } fork_mark;
       thread_config_type thread;
     };
     
+    template <class Coin_flip>
+    machine_config_type step(const Coin_flip& coin_flip, machine_config_type& m) {
+      machine_config_type n;
+      n.tag = Machine_stuck;
+      switch (m.tag) {
+        case Machine_fork_mark: {
+          n.tag = Machine_fork_mark;
+          if (coin_flip()) {
+            *n.fork_mark.m1 = step(coin_flip, *m.fork_mark.m1);
+            n.fork_mark.m2.swap(m.fork_mark.m2);
+          } else {
+            n.fork_mark.m1.swap(m.fork_mark.m1);
+            *n.fork_mark.m2 = step(coin_flip, *m.fork_mark.m2);
+          }
+          break;
+        }
+        case Machine_thread: {
+          thread_config_type& tc_m = m.thread;
+          thread_config_type& tc_n = n.thread;
+          switch (tc_m.t.tag) {
+            case Trace_fork_mark: {
+              n.tag = Machine_fork_mark;
+              auto mp = fork_mark(tc_m.ms);
+              auto rp = fork_mark(tc_m.rs);
+              reference_stack_type rs1, rs2;
+              if (rp.tag == Fork_result_some) {
+                rs1 = rp.s1;
+                rs1.push_back(rp.f1);
+                rs2 = rp.s2;
+                rs2.push_back(rp.f2);
+              } else {
+                rs1 = tc_m.rs;
+              }
+              n.fork_mark.m1.reset(new machine_config_type);
+              n.fork_mark.m2.reset(new machine_config_type);
+              machine_config_type& m1 = *n.fork_mark.m1;
+              machine_config_type& m2 = *n.fork_mark.m2;
+              m1.tag = Machine_thread;
+              m1.thread.t = std::move(*tc_m.t.fork_mark.k1);
+              m1.thread.ms = mp.first;
+              m1.thread.rs = rs1;
+              m2.tag = Machine_thread;
+              m2.thread.t = std::move(*tc_m.t.fork_mark.k2);
+              m2.thread.ms = mp.second;
+              m2.thread.rs = rs2;
+              break;
+            }
+            case Trace_push_back: {
+              frame f = tc_m.t.push_back.f;
+              n.tag = Machine_thread;
+              tc_n.rs = tc_m.rs;
+              tc_n.rs.push_back(f);
+              auto init_fn = [&] (char* p) {
+                new ((frame*)p) frame(f);
+              };
+              tc_n.ms = push_back<sizeof(frame),decltype(init_fn)>(tc_m.ms, f.plt, init_fn);
+              break;
+            }
+            case Trace_pop_back: {
+              n.tag = Machine_thread;
+              tc_n.rs = tc_m.rs;
+              tc_n.rs.pop_back();
+              auto destr_fn = [&] (char* p) {
+                ((frame*)(p))->~frame();
+              };
+              tc_n.ms = pop_back(tc_m.ms, destr_fn);
+              break;
+            }
+            default: {
+              assert(false); // todo
+            }
+          }
+          break;
+        }
+        case Machine_stuck: {
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+      return n;
+    }
+    
     /* Trace */
     /*------------------------------*/
+    
+    void ex1() {
+      frame f;
+      f.v = 123;
+      auto t2 = mk_push_back(f);
+      t2->push_back.k.reset(mk_pop_back());
+      machine_config_type m;
+      step([&] {
+        return rand() % 2 == 0;
+      }, m);
+    }
     
   } // end namespace
 } // end namespace
 
 int main(int argc, const char * argv[]) {
-  std::cout << "Hello, World!\n";
+  std::cout << "starting\n";
+  cactus_stack::basic::ex1();
+  std::cout << "finished\n";
   return 0;
 }
