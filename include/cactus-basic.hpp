a90b4fcc1073be9ed7c2092a6d287e4d33ee0f75
Mike Rainey <mike.rainey@inria.fr>
2017-03-15 15:22:27 +0100

    cleanup

--- include/cactus-basic.hpp
+++ include/cactus-basic.hpp
@@ -257,13 +257,11 @@ namespace cactus_stack {
       }
       p_f1 = p_f2->pred;
       s1.fp = p_f1;
-      chunk_type* c_p_f1 = chunk_of(p_f1);
-      chunk_type* c_p_f2 = chunk_of(p_f2);
-      if (c_p_f1 == c_p_f2) {
-        incr_refcount(c_p_f1);
+      chunk_type* c_f1 = chunk_of(p_f1);
+      if (c_f1 == chunk_of(p_f2)) {
+        incr_refcount(c_f1);
       }
-      chunk_type* c_sp = chunk_of(s.sp);
-      if (c_sp == c_p_f1) {
+      if (chunk_of(s.sp) == c_f1) {
         s1.sp = p_f2;
       } else {
         s1.sp = nullptr;
